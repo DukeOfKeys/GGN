5eae9071bd97919a130289181bae543ca50ef685
kirillinski <kirillinski4@gmail.com>
2024-09-23 21:29:04 +0300

    Added comments to functions

--- GGN.h
+++ GGN.h
@@ -1,5 +1,6 @@
 #ifndef GGN_LIB
 #define GGN_LIB
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,9 +12,26 @@ typedef struct GGN
     unsigned char number[GGN_MAX_NUM_LEN];
     unsigned long long length;
 } GGN;
-int GGN_sum(GGN *n1, GGN *n2, GGN *result);
+/// @brief  Sum of two GGN numbers
+/// @param GGN_n1 first number
+/// @param GGN_n2 second number
+/// @param GGN_result result
+/// @return returns 1 if sucsess
+int GGN_sum(GGN *GGN_n1, GGN *GGN_n2, GGN *GGN_result);
+/// @brief Initialization with const char *
+/// @param string_number number represented by string
+/// @return Returns a pointer to struct with number
 GGN *GGN_init(const char *string_number);
+/// @brief Sets one char to all places in the GGN number
+/// @param GGN_n Pointer
+/// @param n Char (from '0' to '9' reccomended)
 void GGN_set_char_to_all(GGN *GGN_n, unsigned char n);
+/// @brief Prints GGN number
+/// @param GGN_n GGN number
 void GGN_print(GGN *GGN_n);
+/// @brief Sets given int to GGN number
+/// @param GGN_n GGN number
+/// @param number Number that will be handaled (positive numbers reccomended)
+/// @return Operation status
 int GGN_set_int(GGN *GGN_n, int number);
 #endif
