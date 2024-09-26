6ae14335c2264193b12a6ad7c4de17fb50255ca1
dukeofkeys <kirillinski@icloud.com>
2024-09-26 19:30:37 +0300

    Added negative numbers and minus operation

--- GGN.c
+++ GGN.c
@@ -8,10 +8,29 @@ GGN *GGN_i()
     }
     GGN_num->length = 0;
     GGN_set_char_to_all(GGN_num, 0);
+    GGN_num->sign = 0;
     return GGN_num;
 }
 int GGN_sum(GGN *n1, GGN *n2, GGN *result)
 {
+    if (n1->sign && !n2->sign)
+    {
+        n1->sign = 0;
+        GGN_minus(n2, n1, result);
+        if (n1 != result)
+            n1->sign = 1;
+        return 1;
+    }
+    if (n2->sign && !n1->sign)
+    {
+        n2->sign = 0;
+        GGN_minus(n1, n2, result);
+        if (n2 != result)
+            n2->sign = 1;
+        return 1;
+    }
+    if (n1->sign && n2->sign)
+        result->sign = 1;
     if (!(n1 && n2 && result))
         return 0;
     int toBeAdded = 0;
@@ -37,8 +56,13 @@ GGN *GGN_init(const char *string_number)
 {
     GGN *GGN_num = GGN_i();
     GGN_num->length = strlen(string_number);
-    for (int index = 0; index < GGN_num->length; index++)
+    for (int index = 0; (index < GGN_num->length) && (string_number[GGN_num->length - 1 - index] != '-'); index++)
         GGN_num->number[index] = string_number[GGN_num->length - 1 - index] - '0';
+    if (string_number[0] == '-')
+    {
+        GGN_num->length--;
+        GGN_num->sign = 1;
+    }
     return GGN_num;
 };
 void GGN_set_char_to_all(GGN *GGN_n, unsigned char n)
@@ -47,15 +71,43 @@ void GGN_set_char_to_all(GGN *GGN_n, unsigned char n)
 }
 void GGN_print(GGN *GGN_n)
 {
+    if (GGN_n->sign)
+    {
+        printf("-");
+    }
     for (int index = 0; index < GGN_n->length; index++)
     {
         printf("%d", GGN_n->number[GGN_n->length - 1 - index]);
     }
-    printf("\n");
+}
+int GGN_num_str(GGN *GGN_n, char *str, int SIZE)
+{
+    memset(str, 0, SIZE);
+    int add = 0;
+    if (GGN_n->sign)
+    {
+        str[0] = '-';
+        add = 1;
+    }
+    for (int index = 0; (index < GGN_n->length) && (index + add < SIZE); index++)
+    {
+        str[add + index] = GGN_n->number[GGN_n->length - 1 - index] + '0';
+    }
+    return add + GGN_n->length;
 }
 
 int GGN_set_int(GGN *GGN_n, int number)
 {
+
+    if (number < 0)
+    {
+        number *= -1;
+        GGN_n->sign = 1;
+    }
+    else
+
+        GGN_n->sign = 0;
+
     GGN_set_char_to_all(GGN_n, 0);
 
     GGN_n->length = number ? log10(number) + 1 : 1;
@@ -70,6 +122,7 @@ int GGN_set_GGN(GGN *GGN_n1, GGN *GGN_n2)
 {
     GGN_set_char_to_all(GGN_n2, 0);
     GGN_n2->length = GGN_n1->length;
+    GGN_n2->sign = GGN_n1->sign;
     for (int index = 0; index < GGN_n2->length; index++)
         GGN_n2->number[index] = GGN_n1->number[index];
     return 1;
@@ -125,6 +178,8 @@ int GGN_mult(GGN *GGN_n1, GGN *GGN_n2, GGN *GGN_result)
         GGN_sum(GGN_final, GGN_result_oper, GGN_final);
     }
     GGN_set_GGN(GGN_final, GGN_result);
+    if (GGN_n1->sign != GGN_n2->sign)
+        GGN_result->sign = 1;
     return 1;
 }
 int GGN_mult_pow_10(GGN *GGN_n, int power_of_10)
@@ -143,4 +198,48 @@ GGN *GGN_init_int(int num)
     GGN *GGN_num = GGN_i();
     GGN_set_int(GGN_num, num);
     return GGN_num;
+}
+int GGN_minus(GGN *GGN_n1, GGN *GGN_n2, GGN *GGN_result)
+{
+    if (GGN_n1->sign != GGN_n2->sign)
+    {
+        char sign = GGN_n1->sign;
+        GGN_n1->sign = GGN_n2->sign = 0;
+        GGN_sum(GGN_n1, GGN_n2, GGN_result);
+        GGN_n1->sign = sign;
+        GGN_n2->sign = !sign;
+        GGN_result->sign = sign;
+        return 1;
+    }
+    GGN *o1 = GGN_n1, *o2 = GGN_n2;
+    switch (GGN_n1_compare_with_n2(o1, o2))
+    {
+    case 1:
+        break;
+    case 0:
+        GGN_set_int(GGN_result, 0);
+        return 1;
+        break;
+    case -1:
+        o1 = GGN_n2;
+        o2 = GGN_n1;
+        GGN_result->sign = GGN_n2->sign ? 0 : 1;
+        break;
+    }
+    int next = 0;
+    int result = 0;
+    for (int index = 0; (index <= GGN_n1->length || index <= GGN_n2->length) && index < GGN_MAX_NUM_LEN; index++)
+    {
+        result = o1->number[index] - o2->number[index] + next;
+        next = 0;
+        if (result < 0)
+        {
+            next = -1;
+            result += 10;
+        }
+        GGN_result->number[index] = result;
+        if (GGN_result->number[index] > 0)
+            GGN_result->length = index + 1;
+    }
+    return 1;
 }
--- GGN.h
+++ GGN.h
@@ -11,43 +11,65 @@ typedef struct GGN
 {
     unsigned char number[GGN_MAX_NUM_LEN];
     unsigned long long length;
+    unsigned char sign;
 } GGN;
+
 /// @brief  Sum of two GGN numbers
 /// @param GGN_n1 first number
 /// @param GGN_n2 second number
 /// @param GGN_result result
 /// @return returns 1 if sucsess
 int GGN_sum(GGN *GGN_n1, GGN *GGN_n2, GGN *GGN_result);
+
 /// @brief Initialization with const char *
 /// @param string_number number represented by string
 /// @return Returns a pointer to struct with number
 GGN *GGN_init(const char *string_number);
+
 /// @brief Sets one char to all places in the GGN number
 /// @param GGN_n Pointer
 /// @param n Char (from '0' to '9' reccomended)
 void GGN_set_char_to_all(GGN *GGN_n, unsigned char n);
+
 /// @brief Prints GGN number
 /// @param GGN_n GGN number
 void GGN_print(GGN *GGN_n);
+
+int GGN_num_str(GGN *GGN_n, char *str, int SIZE);
+
 /// @brief Sets given int to GGN number
 /// @param GGN_n GGN number
 /// @param number Number that will be handaled (positive numbers reccomended)
 /// @return Operation status
 int GGN_set_int(GGN *GGN_n, int number);
+
 /// @brief Sets GGN_n1 to GGN_n2
 /// @param GGN_n1
 /// @param GGN_n2
 /// @return Operation status
 int GGN_set_GGN(GGN *GGN_n1, GGN *GGN_n2);
+
 /// @brief Comparison of two numbers
 /// @return Returns 1 if n1 > n2, 0 if n1 == n2, -1 if n1 < n2
 int GGN_n1_compare_with_n2(GGN *GGN_n1, GGN *GGN_n2);
+
 int GGN_n_compare_with_int(GGN *GGN_n, int num);
+
 int GGN_n_add_int(GGN *GGN_n, int num, GGN *result);
+
 /// @brief init with no arguments
-/// @return
+/// @return ptr to the struct (memory is allocated only in this function)
 GGN *GGN_i();
+
+/// @brief The most recomended function for init
+/// @param num
+/// @return you know it
 GGN *GGN_init_int(int num);
+
 int GGN_mult_pow_10(GGN *, int power_of_10);
+
 int GGN_mult(GGN *GGN_n1, GGN *GGN_n2, GGN *GGN_result);
+
+int GGN_minus(GGN *GGN_n1, GGN *GGN_n2, GGN *GGN_result);
+
 #endif
